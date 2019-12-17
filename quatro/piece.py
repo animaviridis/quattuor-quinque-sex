d1a235d4039cc47caed51d00a52f9d021014b564
mysh <mysh995@gmail.com>
2019-12-17 21:30:28 +0100

    [Game definition] Piece property - Enum

--- quatro/piece.py
+++ quatro/piece.py
@@ -1,3 +1,25 @@
+from enum import Enum
+
+
+class PieceProperty(int, Enum):
+    def __new__(cls, *args):
+        value = len(cls.__members__)
+        obj = int.__new__(cls, value)
+        obj._value_ = value
+        return obj
+
+    def __init__(self, *args):
+        super().__init__()
+        self._code = self.name[0]
+
+    @property
+    def code(self):
+        return self._code
+
+
+color = PieceProperty('Color', 'Blue Red')
+
+
 class Piece(object):
     """Quatro game piece.
 
