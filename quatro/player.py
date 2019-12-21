19da0d2c56f146cec84d2ad368327b89555e486c
mysh <mysh995@gmail.com>
2019-12-21 12:38:02 +0100

    [Game definition] Player - choosing pieces/coordinates: trivial methods (at random)

--- quatro/player.py
+++
@@ -1,6 +0,0 @@
-class QuatroPlayer(object):
-    """Quatro game player.
-
-    Basic role: takes a string denoting piece type and outputs a position to place it on the board.
-    Outputs a piece type string for the opponent and awaits the coordinates the opponent puts the piece at.
-    """
---
+++ quatro/quatro_player.py
@@ -0,0 +1,32 @@
+import random
+
+from quatro.quatro_board import Board
+
+
+class Player(object):
+    """Quatro game player.
+
+    Basic role: takes a string denoting piece type and outputs a position to place it on the board.
+    Outputs a piece type string for the opponent and awaits the coordinates the opponent puts the piece at.
+    """
+
+    def __init__(self):
+        self._board = Board()
+        self._pick_piece_method = self._pick_piece_random
+        self._pick_field_method = self._pick_field_random
+
+    def pick_piece(self):
+        return self._pick_piece_method()
+
+    def _pick_piece_random(self):
+        return random.choice(self._board.piece_codes_available)
+
+    def pick_field(self):
+        return self._pick_field_method()
+
+    def _pick_field_random(self):
+        return random.choice(self._board.board_fields_available)
+
+
+if __name__ == '__main__':
+    player = Player()
